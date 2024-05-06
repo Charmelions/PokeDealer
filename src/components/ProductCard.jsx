4eb1f4cedd68e608540dc900ef0d0926ed225c76
Char B <charburton18@gmail.com>
2024-05-06 11:39:17 -0700

    created Home Page outline & basic CSS

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,15 @@
+export function ProductCard () {
+  return (
+    <>
+    <div id='productCard'>
+      <div>
+        <img id = 'pokemonImgUrl' src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+      </div>
+      <div>
+        <h2 id='pokemonName'>Bulbasaur</h2>
+        <p id ='pokemonDetails'>$14.99 - Grass Type</p>
+      </div>
+    </div>
+    </>
+  );
+}

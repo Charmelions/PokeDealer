a3e5e53dfe09de43ca3f0b3171a96c010293465d
Find-Serendipity <find.serendipity@gmail.com>
2024-05-06 15:36:31 -0400

    schema updated

--- db/import.cjs
+++ db/import.cjs
@@ -1,18 +1,19 @@
-const API_URL = `https://pokeapi.co/api/v2/pokemon`
+const API_URL = `https://pokeapi.co/api/v2/pokemon`;
+const prisma = require("../client.cjs");
 
 const importAllPokemon = async () => {
   try {
-    const pokemonList = []
-        for (let i = 1; i < 4; i++) {
-          const response = await fetch(`${API_URL}/${i}`);
-          const jsonObject = await response.json();
-          const currentPokemon = jsonObject;
-          pokemonList.push(currentPokemon);
-        }
-        return pokemonList;
-    } catch (err) {
-      console.log('ERROR2 occurred when fetching importAllPokemon', err)
+    const pokemonList = [];
+    for (let i = 1; i < 4; i++) {
+      const response = await fetch(`${API_URL}/${i}`);
+      const jsonObject = await response.json();
+      const currentPokemon = jsonObject;
+      pokemonList.push(currentPokemon);
     }
-  };
+    return pokemonList;
+  } catch (err) {
+    console.log("ERROR2 occurred when fetching importAllPokemon", err);
+  }
+};
 
-module.exports = importAllPokemon
+module.exports = importAllPokemon;

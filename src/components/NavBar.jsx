13158833cf6061d056cf00e001581f8a2d2e5980
Anusha Salman <anushasalman@gmail.com>
2024-05-07 16:30:42 -0500

    initial commit for profile page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ export function NavBar () {
     <>
     <div id='navbarbox'>
       <a href="/">Home </a>
+      <a href="/profile/user_id">Profile Page </a>
       <a href="/register">Register </a>
       <a href="/login">Login </a>
     </div>

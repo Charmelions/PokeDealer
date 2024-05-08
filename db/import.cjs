62d991b0f2eededf35845adeacfe06e51268084c
Find-Serendipity <find.serendipity@gmail.com>
2024-05-07 22:47:34 -0400

    increased pokemon API pull from 3 to 15

--- db/import.cjs
+++ db/import.cjs
@@ -6,7 +6,7 @@ const importAllPokemon = async () => {
   try {
     const pokemonList = [];
     const pokemonDetails = [];
-    for (let i = 1; i < 4; i++) {
+    for (let i = 1; i < 16; i++) {
       const pokemonResponse = await fetch(`${POKE_API_URL}/${i}`);
       const pokemonObject = await pokemonResponse.json();
       const eachPokemon = pokemonObject;
@@ -23,77 +23,6 @@ const importAllPokemon = async () => {
     console.log("ERROR2 occurred when fetching importAllPokemon", err);
   }
 };
-
-// const importStats = async (pokemon) => {
-//   try {
-//     const importedStats = await prisma.stats.create({
-//       data: {
-//         hp: pokemon.stats[0].base_stat,
-//         attack: pokemon.stats[1].base_stat,
-//         defense: pokemon.stats[2].base_stat,
-//         speed: pokemon.stats[5].base_stat,
-//       },
-//     });
-
-//     return importedStats;
-//   } catch (err) {
-//     console.log("Error importing stats", err);
-//     throw err;
-//   }
-// };
-
-// const importType = async (pokemon) => {
-//   try {
-//     const importedType = await prisma.types.create({
-//       data: {
-//         typeOne: pokemon.types[0].type.name,
-//         typeTwo: pokemon.types[1].type.name,
-//       },
-//     });
-
-//     return importedType;
-//   } catch (err) {
-//     console.log("Error importing type", err);
-//     throw err;
-//   }
-// };
-
-// const importSprites = async (pokemon) => {
-//   try {
-//     const importedSprites = await prisma.sprites.create({
-//       data: {
-//         dreamDefault: pokemon.sprites.other.dream_world.front_default,
-//         homeDefault: pokemon.sprites.other.home.front_default,
-//         homeShiny: pokemon.sprites.other.home.front_shiny,
-//         officialDefault:
-//           pokemon.sprites.other["official-artwork"].front_default,
-//         officialShiny: pokemon.sprites.other["official-artwork"].front_shiny,
-//       },
-//     });
-
-//     return importedSprites;
-//   } catch (err) {
-//     console.log("Error importing sprites", err);
-//     throw err;
-//   }
-// };
-
-// const importDescriptions = async (details) => {
-//   try {
-//     const importedDescriptions = await prisma.descriptions.create({
-//       data: {
-//         description: details.flavor_text_entries[0].flavor_text
-//           .replace(/\n/g, " ")
-//           .replace(/\f/g, " "),
-//       },
-//     });
-
-//     return importedDescriptions;
-//   } catch (err) {
-//     console.log("Error importing descriptions", err);
-//     throw err;
-//   }
-// };
 
 const importPokemon = async (pokemon, details) => {
   try {
